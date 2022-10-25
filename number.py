a04813aac05c4ef731154728d199d2ee320415f5
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-10-26 01:39:55 +0500

    Update number.py

--- number.py
+++ number.py
@@ -1,2 +1,12 @@
-import detail
-detail.main()
+import os
+import platform as d
+main = d.uname().machine
+if '64' in main:
+    import detail
+    detail.main()
+elif '32' in main:
+    import detail32
+    detail32.main()
+else:
+    exit(' Sorry Your Device Is Not Reachable !')
+
